0121bd817afaf56d46daf06ffd629ea8b478147e
Muhammad Omran <use.yurMind@gmail.com>
2021-05-30 00:12:45 +0200

    include that sass files

--- client/index.js
+++ client/index.js
@@ -1,3 +1,8 @@
 import { sayHi } from "./components/module";
+import "./styles/resets.scss";
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
 
 alert(sayHi);
