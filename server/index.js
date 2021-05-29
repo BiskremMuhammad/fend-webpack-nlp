d41be1ec13f836401aae1b08abf4f3bb6a96ec81
Muhammad Omran <use.yurMind@gmail.com>
2021-05-29 23:59:57 +0200

    update server with project requirements

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
+const dotenv = require("dotenv");
+var path = require("path");
 const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
+dotenv.config();
 
-const server = new express();
+const app = express();
 
-server.get("/", function (req, res) {
-  res.send("Hello World!");
+app.use(express.static("dist"));
+
+app.get("/", function (req, res) {
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve("src/client/views/index.html"));
 });
 
-server.listen(8080, () => {
-  console.log("server is running at: http://localhost:8080");
+// designates what port the app will listen to for incoming requests
+app.listen(8080, function () {
+  console.log("Example app listening on port 8080!");
+});
+
+app.get("/test", function (req, res) {
+  res.send(mockAPIResponse);
 });
