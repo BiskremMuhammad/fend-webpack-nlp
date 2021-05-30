bf1aa0f39a0940b9894604ae80f21a0608e51ce3
Muhammad Omran <use.yurMind@gmail.com>
2021-05-30 21:47:55 +0200

    send form data to server endpoint and display the results

--- client/components/formHandler.js
+++ client/components/formHandler.js
@@ -1,4 +1,4 @@
-async function handleSubmit(event) {
+function handleSubmit(event) {
   event.preventDefault();
 
   // check what text was put into the form field
@@ -6,22 +6,18 @@ async function handleSubmit(event) {
   Client.checkForName(formText);
   const data = { name: formText };
 
-  try {
-    const res = await fetch("http://localhost:8080/sentiment", {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const results = await res.json();
-    document.getElementById("results").innerHTML = results.message;
-  } catch (er) {
-    console.log("request failed with errors: ", er);
-  }
+  fetch("http://localhost:8081/sentiment", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((results) => {
+      document.getElementById("results").innerHTML = results.message;
+    })
+    .catch((er) => console.log("request failed with errors: ", er));
 }
 
 export { handleSubmit };
