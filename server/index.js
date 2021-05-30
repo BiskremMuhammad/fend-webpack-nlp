627c852c42ba266f22478554c068d2d90d76668f
Muhammad Omran <use.yurMind@gmail.com>
2021-05-30 21:46:11 +0200

    make the request to meaning cloud and respond with analsis data

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 var path = require("path");
 const express = require("express");
-const mockAPIResponse = require("./mockAPI.js");
+const axios = require("axios");
 dotenv.config();
 
 const app = express();
@@ -14,12 +14,28 @@ app.get("/", function (req, res) {
   res.sendFile(path.resolve("dist/index.html"));
 });
 
-app.post("/sentiment", (request, response) => {
-  console.log("rewuest body: ", request.body.name);
-  response.send(mockAPIResponse);
+app.post("/sentiment", async (req, res) => {
+  const requestOptions = {
+    method: "post",
+    url: `https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&txt=${req.body.name}&lang=en`,
+    headers: { "Content-Type": "multipart/form-data" },
+  };
+
+  try {
+    const result = await axios(requestOptions);
+    const { agreement, subjectivity, confidence, irony } = result.data;
+    const message = `Analysis results: Agreement: ${agreement}, Subjectivity: ${subjectivity}, Confidence: ${confidence}, Irony: ${irony}`;
+    res.send({
+      success: true,
+      message,
+    });
+  } catch (err) {
+    console.log("meaning cloud have some trouble: ", err);
+    res.status(500).send({ error: true });
+  }
 });
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-  console.log("Example app listening on port 8080!");
+app.listen(8081, function () {
+  console.log("Example app listening on port 8081!");
 });
