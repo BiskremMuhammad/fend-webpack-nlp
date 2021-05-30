9196c8ea817d279d6a4c75c2fa5ce580230bc85b
Muhammad Omran <use.yurMind@gmail.com>
2021-05-31 01:35:43 +0200

    update the UI if encountered a validation error on the input

--- client/components/formHandler.js
+++ client/components/formHandler.js
@@ -3,21 +3,29 @@ function handleSubmit(event) {
 
   // check what text was put into the form field
   let formText = document.getElementById("name").value;
-  Client.checkForName(formText);
-  const data = { name: formText };
+  let errorIndicator = document.getElementById("errors");
+  document.getElementById("name").classList.remove("warn");
+  errorIndicator.innerHTML = "";
 
-  fetch("http://localhost:8081/sentiment", {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((res) => res.json())
-    .then((results) => {
-      document.getElementById("results").innerHTML = results.message;
+  if (!Client.checkForName(formText)) {
+    document.getElementById("name").classList.add("warn");
+    errorIndicator.innerHTML = "Please enter a valid sentence.";
+  } else {
+    const data = { name: formText };
+
+    fetch("http://localhost:8081/sentiment", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     })
-    .catch((er) => console.log("request failed with errors: ", er));
+      .then((res) => res.json())
+      .then((results) => {
+        document.getElementById("results").innerHTML = results.message;
+      })
+      .catch((er) => console.log("request failed with errors: ", er));
+  }
 }
 
 export { handleSubmit };
--- client/components/nameChecker.js
+++ client/components/nameChecker.js
@@ -3,7 +3,7 @@ function checkForName(inputText) {
   if (!inputText || !inputText.length) {
     return false;
   }
-  if (!inputText.test(/^[A-Za-z\d\s\.-_]+$/g)) {
+  if (!/^[A-Za-z\d\s\.-_,]+$/g.test(inputText)) {
     return false;
   }
   return true;
